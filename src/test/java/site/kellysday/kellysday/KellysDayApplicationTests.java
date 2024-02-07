311fc570434b027a1d73b4aeb2e0c727839bced3
edgar6bf <edgar6bf@gmail.com>
2024-02-08 01:52:13 +0900

    chore: #1 프로젝트 생성
    개발 환경을 생성함

---
+++ src/test/java/site/kellysday/kellysday/KellysDayApplicationTests.java
@@ -0,0 +1,13 @@
+package site.kellysday.kellysday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KellysDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
